85fca53b89975b85779758bdc584a488ab873fc5
Promise <promiseihunna@gmail.com>
2026-04-28 19:35:06 +0100

    feat: implement user authentication logic and update middleware for enhanced security

---
+++ internal/handlers/get_coin_balance.go
@@ -0,0 +1,56 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github/com/promise111/gopi/api"
+
+	"github.com/gorilla/schema"
+	"github.com/promise111/goapi/api"
+	"github.com/promise111/goapi/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
+
+func GetCoinBalance(w http.ResponseWriter, r *http.Request) {
+	var params = api.CoinBalanceParams{}
+	var decoder *schema.Decoder = *schema.NewDecoder()
+	var err error
+
+	err = decoder.Decode(&params, r.URL.Query())
+
+	if err != nil {
+		log.Error(err)
+		api.RequestErrorHandler(w, err)
+		return
+	}
+
+	var database *tools.DatabaseInterface
+	database, err = tools.NewDatabase()
+	if err != nil {
+		api.InternalErrorHandler(w)
+		return
+	}
+
+	var tokenDetails *tools.CoinDetails
+	tokenDetails = (*database).GetUserCoins(params.Username)
+	if tokenDetails == nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+
+	var response = api.CoinBalanceResponse{
+		Balance: (*tokenDetails).Coins,
+		Code: http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+}
